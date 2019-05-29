b61a0c77a5ea271b6abafce9c3f20d3bac8c1a17
Audrey Detsaboun <audrey.detsaboun@gmail.com>
2019-05-29 20:57:30 +0200

    require pour dbConnexion

--- models/modelClients.php
+++ models/modelClients.php
@@ -2,18 +2,7 @@
 /****** page Model pour accéder à la base de données 
  * et faire une recherche SQL spécifique ******/
  
-try
-{
-	//db connexion
-    function getBdd(){
-        $bdd = new PDO ('mysql:host=localhost;dbname=ChloeBdd;charset=utf8', 'root', 'root');
-        return $bdd;
-    }
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+require_once('dbConnexion.php');
 
 
 
